0cc8b3b847423d43df8e6ea540cf0d3c1ef7b10a
CarolineFang113 <ytf113@uw.edu>
2023-02-20 15:17:50 -0800

    add by Caroline

--- Chart 1.R
+++ Chart 1.R
@@ -1,13 +1,18 @@
 # Chart 1
 library(ggplot2)
+library(dplyr)
 
 students_in_math <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-AG-Group6/main/student-mat.csv", stringsAsFactors = FALSE)
-  ggplot(data = students_in_math) +
-  geom_boxplot(aes(x = freetime, y = Dalc + Walc, group = freetime)) +
+new_students_in_math <- 
+  students_in_math %>%
+  mutate(amount_alcohol = Dalc + Walc)
+
+ggplot(data = new_students_in_math) +
+  geom_boxplot(aes(x = freetime, y = amount_alcohol, group = freetime)) +
   labs(
     title = "The relationship between student alcohol consumption and free time afterschool",
     x = "Free Time Afterschool",
     y = "The Amount Of Student Alcohol Consumption"
-     )
+  )
 
   
