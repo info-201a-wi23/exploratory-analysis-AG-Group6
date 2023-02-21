a338bf65b8d794976e3dcbc6a93c71b43a580818
Josefinaalbert <josefinaja10@yahoo.com>
2023-02-20 19:28:58 -0800

    added scatterplot code chunk

--- Chart2.R
+++ Chart2.R
@@ -4,10 +4,9 @@ math_df <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/explorator
 
 alc_by_age <- math_df %>% group_by(age) %>% summarize(average = mean(Dalc))
 
-Age <- ggplot(alc_by_age) +
+ggplot(alc_by_age) +
   geom_point(mapping = aes(x = age, y = average)) +
   labs(title = "Avg Alcohol Consumption Per Age", x = "Student Age", y = "Avg. Reported Consumption") +
   theme(panel.background = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
-
